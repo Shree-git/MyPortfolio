3dbfc6cebf783957e51ef138f0d5c705d130d878
Shree-git <sridhar.pandey1@gmail.com>
2022-03-15 17:00:42 -0500

    First version of portfolio

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,312 +1,185 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProfileService {
   //baseUrl = environment.baseUrl;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
-  projects:any = [
+  projects: any = [
     {
       id: 1,
-      title: 'Read data from Google spreadsheet in Angular',
-      desc: '',
-      livedemo: 'https://mehulk05.github.io/MyShop/#/',
-      githurl: 'https://github.com/mehulk05/MyShop',
-      mediumlink: 'https://itnext.io/read-data-from-google-spreadsheet-in-angular-209ee74b6cb4',
-      imgUrl: 'assets/images/z1.png',
-      tech: 'Angular 10 , Google SpreadsheetApi'
+      title: "Choices",
+      subtitle: "An app to challenge addiction",
+      desc: `A cross-platform app for iOS, Android, Desktop and Web which help patients recover from drug and alcohol addiction.`,
+      livedemo: "https://choicesnew-a0046.web.app/",
+      githurl: "https://github.com/mehulk05/MyShop",
+      mediumlink: "",
+      imgUrl: "assets/images/z1.png",
+      tech: "Ionic, Node, Angular, Firebase",
     },
 
     {
       id: 2,
-      title: 'Blog App Using MERN Stack',
-      desc: '',
-      livedemo: 'https://mehulk05.github.io/Blogapp-using-MERN/#/',
-      githurl: 'https://github.com/mehulk05/Blogapp-using-MERN',
-      mediumlink: 'https://itnext.io/blog-app-using-mern-stack-b0b4d69d7ea1',
-      imgUrl: 'assets/images/z26.png',
-      tech: 'React, Node, MongoDB, Express'
+      title: "COVID Hero",
+      subtitle: "",
+      desc: `An all-in-one app that keeps users up to date on the spread of Covid-19 while offering features like testing 
+      locations, future prediction system, GPS hotzone tracker and crowd-sourced reviews for businesses on how COVID safe 
+      they are`,
+      livedemo: "https://covid-detection-37cda.web.app/",
+      githurl: "https://github.com/Shree-git/covid-detection-team08/",
+      mediumlink:
+        "https://devpost.com/software/covid-hero-covid-detection-team-8/",
+      imgUrl: "assets/images/z26.png",
+      tech: "Ionic, Node, Angular, Firebase, R, Python, Google Maps API",
     },
-
 
     {
       id: 3,
-      title: 'CRUD operation using MEAN stack',
-      desc: '',
-      livedemo: 'https://mehulk05.github.io/Blog-using-mean/#/',
-      githurl: 'https://github.com/mehulk05/Blog-using-mean',
-      mediumlink: 'https://itnext.io/crud-operation-using-mean-stack-7dfa2f51ec8c',
-      imgUrl: 'assets/images/z27.png',
-      tech: 'Angular 10, Node, MongoDB, Express'
+      title: "COMPS",
+      subtitle: "",
+      desc: `A web-chat application which online teaching and learning easier for students, professors, and TAs`,
+      livedemo: "https://comps-ea6c1.web.app/login-chat",
+      githurl: "https://github.com/Shree-git/COMPS-Chat-Engine",
+      mediumlink:
+        "",
+      imgUrl: "assets/images/z27.png",
+      tech: "Angular, Node, Firebase",
     },
-
-    {
-      id: 4,
-      title: 'Multiroom Chat Application in Node JS',
-      desc: '',
-      livedemo: 'https://chatapp-using-node.herokuapp.com/',
-      githurl: 'https://github.com/mehulk05/Chat-app-using-Nodejs',
-      mediumlink: 'https://medium.com/weekly-webtips/building-a-multiroom-chat-application-in-node-js-8a8adca5acf2',
-      imgUrl: 'assets/images/z23.png',
-      tech: 'Node JS,Socket IO'
-    },
-
-
-    {
-      id: 5,
-      title: 'Integrate Medium Blogs on your React App',
-      desc: '',
-      livedemo: 'https://mehulk05.github.io/Medium-Blog-On-React-App#/',
-      githurl: 'https://github.com/mehulk05/Medium-Blog-On-React-App',
-      mediumlink: 'https://javascript.plainenglish.io/how-to-add-medium-blogs-on-your-react-app-a3898a729aad',
-      imgUrl: 'assets/images/z4.png',
-      tech: 'React JS'
-    },
-
-
-
-    {
-      id: 6,
-      title: 'News App using ReactJS (Parsing XML to JSON)',
-      desc: '',
-      livedemo: 'https://mehulk05.github.io/NewsApp-using-React',
-      githurl: 'https://github.com/mehulk05/NewsApp-using-React',
-      mediumlink: 'https://codeburst.io/news-app-using-reactjs-f9752d7ab6ce',
-      imgUrl: 'assets/images/z24.png',
-      tech: 'React JS, XMl to JSON'
-    },
-
-
-
-    {
-      id: 7,
-      title: 'Medium-Like Blog App Using Angular 9 and Firebase',
-      desc: '',
-      livedemo: 'https://mehulk05.github.io/Blog-Book/',
-      githurl: 'https://github.com/mehulk05/Blog-Book',
-      mediumlink: 'https://codeburst.io/blogging-app-using-angular-9-and-firebase-98e36168d850',
-      imgUrl: 'assets/images/z21.png',
-      tech: 'HTML, Bootstrap, CSS, Angular-9, Firebase'
-    },
-
-
-
-    {
-      id: 8,
-      title: 'COVID 19 Tracker (Statistics) app Using Angularr',
-      desc: '',
-      livedemo: 'https://mehulk05.github.io/COVOID-19/',
-      githurl: 'https://github.com/mehulk05/COVOID-19',
-      mediumlink: 'https://codeburst.io/covoid-19-tracker-statistics-app-using-angular-2358347b3d1f',
-      imgUrl: 'assets/images/z2.png',
-      tech: 'Angular 10 ,Bootstrap,Javascript'
-    },
-
-
-    {
-      id: 9,
-      title: 'Weather App with React',
-      desc: '',
-      livedemo: 'https://mehulk05.github.io/React-Weather/',
-      githurl: 'https://github.com/mehulk05/React-Weather-App',
-      mediumlink: 'https://medium.com/analytics-vidhya/weather-app-with-react-adaa7268bbdb',
-      imgUrl: 'assets/images/z9.png',
-      tech: 'React, Open MAp Api'
-    },
-
-
-    {
-      id: 10,
-      title: 'Spring Boot + Angular 8 CRUD Example',
-      desc: '',
-      livedemo: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      githurl: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
-      mediumlink: 'https://medium.com/analytics-vidhya/spring-boot-angular-8-crud-example-8aeafd47b54',
-      imgUrl: 'assets/images/z28.png',
-      tech: 'Angular8, Java ,Hibernate'
-    },
-
-
-    {
-      id: 11,
-      title: 'Personal Portfolio Using Angular',
-      desc: '',
-      livedemo: 'https://personal-portfolio0320.000webhostapp.com/',
-      githurl: 'https://github.com/mehulk05/Personal-Portfolio-Angular-Updated',
-      mediumlink: 'https://javascript.plainenglish.io/personal-portfolio-using-angular-8-and-bootstrap-5dde6fc7cf9b',
-      imgUrl: 'assets/images/z11.png',
-      tech: 'Angular ,Bootstrap'
-    },
-
-
-    {
-      id: 12,
-      title: 'Personal Portfolio Using Django',
-      desc: '',
-      livedemo: 'https://personal-portfolio0320.000webhostapp.com/',
-      githurl: 'https://github.com/mehulk05/My-Personal-Portfolio',
-      mediumlink: 'https://mehulkothari05.medium.com/personal-portfolio-using-django-dba49c355905',
-      imgUrl: 'assets/images/z12.png',
-      tech: 'Django'
-    },
-
-    {
-      id: 13,
-      title: 'Movie Success Prediction',
-      desc: '',
-      livedemo: 'https://mehulkothari05.medium.com/',
-      githurl: 'https://github.com/mehulk05',
-      mediumlink: 'https://mehulkothari05.medium.com/movie-success-prediction-a67d871a7b1',
-      imgUrl: 'assets/images/z13.png',
-      tech: 'Python, Machine Learning, Django Framework.'
-    },
-
-    {
-      id: 14,
-      title: 'Matrimonial Website',
-      desc: '',
-      livedemo: 'https://mehulkothari05.medium.com/',
-      githurl: 'https://github.com/mehulk05/Matrimonial-Website.git',
-      mediumlink: 'https://mehulkothari05.medium.com/matrimonial-website-9cd5247f477d',
-      imgUrl: 'assets/images/z14.png',
-      tech: 'HTML, CSS, Bootstrap, Jquery'
-    },
-
-  ]
+  ];
   about2 = `Software Developer with 2.5+ years of experience .
   Worked with Software Development Life Cycle(SDLC) and  agile methodologies .
   Expertise in Angular 2+ All versions, React, Javascript,  Node JS and UI with Responsive Designs .
-  Have good understanding of Python, Jquery, Bootstrap, MongoDB, Firebase, and MYSQL`
+  Have good understanding of Python, Jquery, Bootstrap, MongoDB, Firebase, and MYSQL`;
 
-  about = "Lover of innovation and everything related to generate new knowledge.Face problems with a smile and solve them as soon as possible. Very calculated about the time I spend and work I do."
-  resumeurl = "https://drive.google.com/file/d/1XY0xlYDX8aZA1KwCnnjH6Y3G4iTWcV-D/view?usp=sharing"
+  about =
+    "Lover of innovation and everything related to generate new knowledge.Face problems with a smile and solve them as soon as possible. Very calculated about the time I spend and work I do.";
+  resumeurl =
+    "https://drive.google.com/file/d/1h2WcqicqCEQcu2ZtG8HRQtimjRLcU0N_/view?usp=sharing";
 
   skillsData: any = [
     {
-      'id': '1',
-      'skill': 'ANGULAR 2+',
-      'progress': '85%'
+      id: "6",
+      skill: "PYTHON, JAVA, C#, Ruby",
+      progress: "80%",
     },
     {
-      'id': '2',
-      'skill': 'NODE JS',
-      'progress': '80%'
+      id: "1",
+      skill: "ANGULAR",
+      progress: "85%",
     },
     {
-      'id': '3',
-      'skill': 'JAVASCRIPT JQUERY',
-      'progress': '80%'
+      id: "2",
+      skill: "NODE JS, JAVASCRIPT",
+      progress: "80%",
     },
     {
-      'id': '4',
-      'skill': 'SQL , MYSQL,MONOGO, FIREBASE',
-      'progress': '75%'
+      id: "3",
+      skill: "Ruby on Rails",
+      progress: "80%",
     },
     {
-      'id': '5',
-      'skill': 'REACT JS',
-      'progress': '75%'
+      id: "4",
+      skill: "SQL, MYSQL, MONOGODB, FIREBASE",
+      progress: "75%",
     },
     {
-      'id': '6',
-      'skill': 'PYTHON,JAVA',
-      'progress': '70%'
-    }
+      id: "5",
+      skill: "REACT",
+      progress: "75%",
+    },
+    {
+      id: "7",
+      skill: "AI, Machine Learning, Deep Learning, Neural Networks",
+      progress: "65%",
+    },
   ];
-
 
   educationData: any = [
     {
-      'id': '1',
-      'from_to_year': '2015 - 2019',
-      'education': 'Bachelor\'s Degree',
-      'stream': 'Bachelor of Engineering',
-      'info': `Aditya Silver Oak Institute is GTU Affiliated and ranks 7th in Gujurat
+      id: "1",
+      from_to_year: "2015 - 2019",
+      education: "Bachelor's Degree",
+      stream: "Bachelor of Engineering",
+      info: `Aditya Silver Oak Institute is GTU Affiliated and ranks 7th in Gujurat
 Completed B.E in Computer Engineering with 9.54 CGPA.  
 Won the Best Student Award 3 times for excellent Academic records at College. 
 Maintained above 9.1 spi in every semester of college academic .`,
-      'institution': 'ADITYA SILVER OAK INSTITUTE OF TECHNOLOGY, AHMEDABAD'
+      institution: "ADITYA SILVER OAK INSTITUTE OF TECHNOLOGY, AHMEDABAD",
     },
     {
-      'id': '2',
-      'from_to_year': '2013 - 2015',
-      'education': 'Higher Secondary',
-      'stream': 'Science and Mathematics',
-      'institution': 'GYANDEEEP VIDHYALAYA,AHMEDABAD',
-      'info': `The High School was mainly focus on the bases of science, mathematics, and Chemistry
+      id: "2",
+      from_to_year: "2013 - 2015",
+      education: "Higher Secondary",
+      stream: "Science and Mathematics",
+      institution: "GYANDEEEP VIDHYALAYA,AHMEDABAD",
+      info: `The High School was mainly focus on the bases of science, mathematics, and Chemistry
 There was also a good base on physics, mathematics and chemistry.
-Completed my high school with 78%.`
+Completed my high school with 78%.`,
     },
     {
-      'id': '3',
-      'from_to_year': '2012 - 2013',
-      'education': 'Secondary  School',
-      'stream': 'Science and Mathematics',
-      'institution': 'GYANDEEEP VIDHYALAYA,AHMEDABAD',
-      'info': `The Secondary aims at Maths , English , Science, Social Science, Litrature and Languages.
-Completed my Secondary school with 84%.`
-    }
+      id: "3",
+      from_to_year: "2012 - 2013",
+      education: "Secondary  School",
+      stream: "Science and Mathematics",
+      institution: "GYANDEEEP VIDHYALAYA,AHMEDABAD",
+      info: `The Secondary aims at Maths , English , Science, Social Science, Litrature and Languages.
+Completed my Secondary school with 84%.`,
+    },
   ];
   exprienceData: any = [
     {
-      id: 4,
-      company: 'Crest Data System',
-      location: 'Ahmedabad',
-      timeline: 'Aug 2021 to Present',
-      role: 'Software Developer',
-      work: 'Working as a full stack developer .Responsible for handling the UI in React and Managing the api calls in python Flask.'
-
+      id: 5,
+      company: "Woven",
+      location: "Indianapolis, IN (Remote)",
+      timeline: "June 2021 - Aug 2021",
+      role: "Software Engineering Intern",
+      work: `Used React and Ruby on Rails to maintain a full-stack application which helped over 30 customers make their hiring process efficient and find hidden gems within a pool of applicants.
+      Assisted in the process of increasing the company’s annual ARR by 116% and 3x its revenue`,
     },
     {
+      id: 4,
+      company: "Techpoint",
+      location: "Indianapolis, IN (Remote)",
+      timeline: "June 2020 - July 2020",
+      role: "Software Engineering S.O.S. Challenge Intern",
+      work: `Worked as a project manager and software developer on a multi-disciplinary team to create a solution for the challenges faced during COVID-19 pandemic. Won the ‘Fan Favorite’ award.
+      Built features like self-assessment test, testing locations, daily COVID updates, prediction system, GPS hotzone tracker and crowd-sourced reviews for businesses on how COVID safe they are.`,
+    },
+
+    {
       id: 3,
-      company: 'Indianic Info Tech',
-      location: 'Ahmedabad',
-      timeline: 'Feb 2021 to June 2021',
-      role: 'Software Developer',
-      work: `Technologies : Angular , Node JS , MongoDB . 
-      Worked on the web application built in Angular for different business and Campaigns . 
-      Contributed on developing the admin dashboard with different charts and insights for the application.`
+      company: "Valparaiso University",
+      location: "Valparaiso, IN",
+      timeline: "Aug 2019 - Present",
+      role: "Ambassador in Admissions",
+      work: `Give tour of the University to prospective students and families.
+      Phone prospective students and guide them through their college search process.`,
     },
 
     {
       id: 2,
-      company: 'Growth99',
-      location: 'Remote',
-      timeline: '',
-      role: 'Frontend  Developer',
-      work: `Technologies : Angular , Node JS , MongoDB . 
-  Worked on the product of Growth99 which allowed other clients to come and integrate our product in their website . 
-  Worked on REST Api , Admin Dashboard , Appointment Booking for services and UI of the application . 
-  Created sections like templates for email, sms, landing pages, clinic, services, providers and triggers Automation. 
-  `
+      company: "Valparaiso University",
+      location: "Valparaiso, IN",
+      timeline: "Aug 2020 - Present",
+      role: "Teaching Assistant (TA)",
+      work: `Assist the professor with the course and hold office hours to work one-on-one with students`,
     },
 
     {
       id: 1,
-      company: 'Tata Consultancy Services',
-      location: 'Gandhinagar and Pune',
-      timeline: 'June 2019 - Jan 2021',
-      role: 'Assistant System Engineer Trainee',
-      work: `Technologies : Angular , JavaSpring Boot , SQL. Apache Solr . 
-  
-  Project 1 : Worked on uploading two excel sheets with bulk of data and finding the matches for each business . 
-  
-  Project 2 : Worked on indexing the search results inside the application with bulk of data using Apache Solr and configuring the server with cron jobs and shell scripts . 
-  
-  Worked on ranking the search results based on various criteria,features and delivering the accurate results based on generated scores.
-  `
+      company: "Duesenberg Welcome Center",
+      location: "Valparaiso, IN",
+      timeline: "Aug 2019 - Present",
+      role: "Front Desk Attendant",
+      work: `Coordinate tour guides and the prospective families for the campus tour and provide customer support`,
     },
-
-
-  ]
+  ];
   // contactus(data: any): Observable<any> {
   //   return this.http.post(this.baseUrl + 'contact', data);
   // }
